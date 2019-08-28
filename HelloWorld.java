be75a77d23df744e317ceadb0ddd59960911b2da
Daniel Kim <ddk1@andrew.cmu.edu>
2019-08-28 13:06:31 -0400

    Should have created a working hello world program and also added the number of years I have been at CMU to the print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,5 @@
-public static void main(args String[]) {
-	System.out.println("Hello World?");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World? I have been at CMU for two years");
+    }
 }
