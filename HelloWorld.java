6fb3bd9023002a2670a95ba22d973a29ddae661b
Brandon Jiang <brjiang@Brandons-MacBook-Pro.local>
2019-08-28 13:20:21 -0400

    add year

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World? I have been at CMU for two years");
+        System.out.println("Hello World? I have been at CMU for two years. The year is 2019");
     }
 }
